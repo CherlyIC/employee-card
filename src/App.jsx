a019d31b8095978ed2c4e0e3ae9e135ea5fb873f
CherlyIC <cherlyimfurankunda@gmail.com>
2026-03-06 11:48:59 +0200

    initialize  our react app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  const title = "Employee Card"
+  const employee = "Cherly"
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{employee}</p>
+    </div>
+  )
+} 
+export default App
