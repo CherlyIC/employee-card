c50419aefa1118d23b99f466ce4187e5f6185269
CherlyIC <cherlyimfurankunda@gmail.com>
2026-03-07 10:54:12 +0200

    how to add avatar on my card

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 function EmployeeCard({ id,name, email, phone, website, company}) {
   return (
     <div className='bg-white rounded-xl shadow-md p-6 hover:shadow-xl transition-shadow duration-300'>
+      <img src={`https://api.dicebear.com/7.x/initials/svg?seed=${name}`} alt={name} 
+      className="w-16 h-16 rounded-full mb-4"
+      />
       <p>ID: {id}</p>
       <h2 className='text-xl font-bold text-gray-800 mt-2'>{name}</h2>
       <p className='text-sm text-gray-500 mt-2'>{email}</p>
